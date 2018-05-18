fd9eb3dbdfd9b970d81ef70370f87b2bac50df92
Raymond Zhang <raymond.zhang@brookfieldrps.com>
2018-05-18 16:00:06 -0400

    Add truffle test to launch

--- test/TestMetacoin.sol
+++ test/TestMetacoin.sol
@@ -22,17 +22,3 @@ contract TestMetacoin {
     Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
   }
 }
-
-var account_one = "0x627306090abab3a6e1400e9345bc60c78a8bef57"; // an address
-var account_two = "0xf17f52151ebef6c7334fad080c5704d77216b732"; // another address
-
-var meta;
-PhotoOwnership.deployed().then(function(instance) {
-  meta = instance;
-  return meta.transfer(account_two, 10, {from: account_one});
-}).then(function(result) {
-  // If this callback is called, the transaction was successfully processed.
-  console.log("Transaction successful!")
-}).catch(function(e) {
-  console.log(e)
-})
